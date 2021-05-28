b531c8ea8979725f3f1a7564dfb6c675e2a5508d
bored-taiga <bocskai.laszlo@outlook.com>
2021-05-28 12:51:19 +0200

    First

---
+++ android/app/src/main/kotlin/com/uxper/caribbeanamericanconnexion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.uxper.caribbeanamericanconnexion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/modules/services/platform/lara/lara.dart
@@ -0,0 +1,7 @@
+import 'package:Golo/modules/services/platform/PlatformBase.dart';
+
+class Lara extends PlatformBase {
+  Lara(): super(){
+    baseUrl = "";
+  }
+}
